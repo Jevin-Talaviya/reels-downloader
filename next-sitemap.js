56f6a82a56002366b97e7098cdc36393e9208333
Talaviya Jevin <146707487+Jevin-Talaviya@users.noreply.github.com>
2025-02-04 16:15:47 +0530

    initial commit

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourdomain.com',
+    generateRobotsTxt: true, // This will generate robots.txt for you
+  };
+  
