1a48a01fda47d436f199bf69136066249b4e548f
Talaviya Jevin <146707487+Jevin-Talaviya@users.noreply.github.com>
2025-02-13 19:30:13 +0530

    Sitemape change

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -4,35 +4,31 @@ export async function GET() {
   const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://https://reelsdownloader.techs/</loc>
+        <loc>https://reelsdownloader.tech/</loc>
         <lastmod>2025-02-08</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
-    
     <url>
-        <loc>https://https://reelsdownloader.tech/how-to-download</loc>
+        <loc>https://reelsdownloader.tech/how-to-download</loc>
         <lastmod>2025-02-08</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
-    
     <url>
-        <loc>https://https://reelsdownloader.tech/features</loc>
+        <loc>https://reelsdownloader.tech/features</loc>
         <lastmod>2025-02-08</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
-    
     <url>
-        <loc>https://https://reelsdownloader.tech/faq</loc>
+        <loc>https://reelsdownloader.tech/faq</loc>
         <lastmod>2025-02-08</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.7</priority>
     </url>
-    
     <url>
-        <loc>https://https://reelsdownloader.tech/contact</loc>
+        <loc>https://reelsdownloader.tech/contact</loc>
         <lastmod>2025-02-08</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.6</priority>
